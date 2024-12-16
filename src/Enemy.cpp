b16ceb2be689c0c576bd9c7d66a427542f298d67
Pascra <arnaupascual40@gmail.com>
2024-12-16 09:33:10 +0100

    Enemigo persigue al jugador y debug key para dibujar el pathfinding

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -11,181 +11,190 @@
 #include "EntityManager.h"
 #include "tracy/Tracy.hpp"
 
-Enemy::Enemy() : Entity(EntityType::ENEMY)
+Enemy::Enemy() : Entity(EntityType::ENEMY), pathfinding(nullptr), pbody(nullptr)
 {
-
 }
 
 Enemy::~Enemy() {
-	delete pathfinding;
+    if (pathfinding) {
+        delete pathfinding;
+    }
 }
 
 bool Enemy::Awake() {
-	return true;
+    return true;
 }
 
 bool Enemy::Start() {
+    // Initialize flag for path drawing
+    DrawPathFlag = false;
 
+    // Initialize textures
+    texture = Engine::GetInstance().textures.get()->Load(parameters.attribute("texture").as_string());
+    position.setX(parameters.attribute("x").as_int());
+    position.setY(parameters.attribute("y").as_int());
+    texW = parameters.attribute("w").as_int();
+    texH = parameters.attribute("h").as_int();
 
+    // Load animations
+    idle.LoadAnimations(parameters.child("animations").child("idle"));
+    currentAnimation = &idle;
 
-	//initilize textures
-	texture = Engine::GetInstance().textures.get()->Load(parameters.attribute("texture").as_string());
-	position.setX(parameters.attribute("x").as_int());
-	position.setY(parameters.attribute("y").as_int());
-	texW = parameters.attribute("w").as_int();
-	texH = parameters.attribute("h").as_int();
+    // Initialize physics body
+    pbody = Engine::GetInstance().physics.get()->CreateCircle((int)position.getX() + texH / 2, (int)position.getY() + texH / 2, texH / 2, bodyType::DYNAMIC);
 
-	//Load animations
-	idle.LoadAnimations(parameters.child("animations").child("idle"));
-	currentAnimation = &idle;
+    // Assign collider type
+    pbody->ctype = ColliderType::ENEMY;
+    pbody->listener = this;
 
-	//Add a physics to an item - initialize the physics body
-	pbody = Engine::GetInstance().physics.get()->CreateCircle((int)position.getX() + texH / 2, (int)position.getY() + texH / 2, texH / 2, bodyType::DYNAMIC);
+    // Set gravity for the physics body
+    bool gravity = parameters.attribute("gravity").as_bool(); // Store gravity in a variable to avoid errors
+    if (!gravity) pbody->body->SetGravityScale(0);
 
-	//Assign collider type
-	pbody->ctype = ColliderType::ENEMY;
+    // Initialize pathfinding
+    pathfinding = new Pathfinding();
+    ResetPath();
 
-	pbody->listener = this;
-
-	// Set the gravity of the body
-	if (!parameters.attribute("gravity").as_bool()) pbody->body->SetGravityScale(0);
-
-	// Initialize pathfinding
-	pathfinding = new Pathfinding();
-	ResetPath();
-
-	return true;
+    return true;
 }
 
-bool Enemy::Update(float dt)
-{
-	ZoneScoped;
-	// Pathfinding testing inputs
-	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_R) == KEY_DOWN) {
-		Vector2D pos = GetPosition();
-		Vector2D tilePos = Engine::GetInstance().map.get()->WorldToMap(pos.getX(), pos.getY());
-		pathfinding->ResetPath(tilePos);
-	}
+bool Enemy::Update(float dt) {
+    ZoneScoped;
 
-	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_J) == KEY_DOWN) {
-		pathfinding->PropagateBFS();
-	}
+    // Testing pathfinding inputs for various algorithms
+    if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_R) == KEY_DOWN) {
+        Vector2D pos = GetPosition();
+        Vector2D tilePos = Engine::GetInstance().map.get()->WorldToMap(pos.getX(), pos.getY());
+        pathfinding->ResetPath(tilePos);
+    }
 
-	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_J) == KEY_REPEAT &&
-		Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT) {
-		pathfinding->PropagateBFS();
-	}
+    if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_J) == KEY_DOWN) {
+        pathfinding->PropagateBFS();
+    }
 
-	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_K) == KEY_DOWN) {
-		pathfinding->PropagateDijkstra();
-	}
+    if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_J) == KEY_REPEAT && Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT) {
+        pathfinding->PropagateBFS();
+    }
 
-	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_K) == KEY_REPEAT &&
-		Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT) {
-		pathfinding->PropagateDijkstra();
-	}
+    if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_K) == KEY_DOWN) {
+        pathfinding->PropagateDijkstra();
+    }
 
-	// L13: TODO 3:	Add the key inputs to propagate the A* algorithm with different heuristics (Manhattan, Euclidean, Squared)
+    if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_K) == KEY_REPEAT && Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT) {
+        pathfinding->PropagateDijkstra();
+    }
 
-	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_B) == KEY_DOWN) {
-		pathfinding->PropagateAStar(MANHATTAN);
-	}
+    if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_B) == KEY_DOWN) {
+        pathfinding->PropagateAStar(MANHATTAN);
+    }
 
-	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_B) == KEY_REPEAT &&
-		Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT) {
-		pathfinding->PropagateAStar(MANHATTAN);
-	}
+    if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_B) == KEY_REPEAT && Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT) {
+        pathfinding->PropagateAStar(MANHATTAN);
+    }
 
-	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_N) == KEY_DOWN) {
-		pathfinding->PropagateAStar(EUCLIDEAN);
-	}
+    if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_N) == KEY_DOWN) {
+        pathfinding->PropagateAStar(EUCLIDEAN);
+    }
 
-	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_N) == KEY_REPEAT &&
-		Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT) {
-		pathfinding->PropagateAStar(EUCLIDEAN);
-	}
+    if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_N) == KEY_REPEAT && Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT) {
+        pathfinding->PropagateAStar(EUCLIDEAN);
+    }
 
-	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_M) == KEY_DOWN) {
-		pathfinding->PropagateAStar(SQUARED);
-	}
+    if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_M) == KEY_DOWN) {
+        pathfinding->PropagateAStar(SQUARED);
+    }
 
-	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_M) == KEY_REPEAT &&
-		Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT) {
-		pathfinding->PropagateAStar(SQUARED);
-	}
+    if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_M) == KEY_REPEAT && Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT) {
+        pathfinding->PropagateAStar(SQUARED);
+    }
 
-	// L08 TODO 4: Add a physics to an item - update the position of the object from the physics.  
-	b2Transform pbodyPos = pbody->body->GetTransform();
-	position.setX(METERS_TO_PIXELS(pbodyPos.p.x) - texH / 2);
-	position.setY(METERS_TO_PIXELS(pbodyPos.p.y) - texH / 2);
+    // Reset pathfinding and find the path
+    ResetPath();
+    while (pathfinding->pathTiles.empty()) {
+        pathfinding->PropagateAStar(SQUARED);
+    }
 
-	Engine::GetInstance().render.get()->DrawTexture(texture, (int)position.getX(), (int)position.getY(), &currentAnimation->GetCurrentFrame());
-	currentAnimation->Update();
+    // Move enemy based on pathfinding
+    Vector2D posPath;
+    for (int i = 0; i < pathfinding->pathTiles.size() - 1; i++) {
+        posPath = pathfinding->pathTiles.front();
+        pathfinding->pathTiles.remove(posPath);
+    }
 
-	// Draw pathfinding 
-	
-		pathfinding->DrawPath();
-	
-	
+    // Update enemy's position using physics
+    Vector2D posEnemyTile = Engine::GetInstance().map->WorldToMap(position.getX(), position.getY());
+    if (posPath.getX() < posEnemyTile.getX()) pbody->body->SetLinearVelocity(b2Vec2(-1.4f, 0));
+    if (posPath.getX() > posEnemyTile.getX()) pbody->body->SetLinearVelocity(b2Vec2(1.4f, 0));
 
-	return true;
+    // Update position based on physics body transform
+    b2Transform pbodyPos = pbody->body->GetTransform();
+    position.setX(METERS_TO_PIXELS(pbodyPos.p.x) - texH / 2);
+    position.setY(METERS_TO_PIXELS(pbodyPos.p.y) - texH / 2);
+
+    // Draw enemy texture
+    Engine::GetInstance().render.get()->DrawTexture(texture, (int)position.getX(), (int)position.getY(), &currentAnimation->GetCurrentFrame());
+    currentAnimation->Update();
+
+    // Toggle DrawPathFlag when F7 is pressed
+    if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_F7) == KEY_DOWN) {
+        DrawPathFlag = !DrawPathFlag;
+    }
+
+    // Draw path if the flag is set
+    if (DrawPathFlag) {
+        pathfinding->DrawPath();
+    }
+
+    return true;
 }
 
-bool Enemy::CleanUp()
-{
-	Engine::GetInstance().physics.get()->DeletePhysBody(pbody);
-	return true;
+bool Enemy::CleanUp() {
+    Engine::GetInstance().physics.get()->DeletePhysBody(pbody);
+    return true;
 }
 
 void Enemy::SetPosition(Vector2D pos) {
-	pos.setX(pos.getX() + texW / 2);
-	pos.setY(pos.getY() + texH / 2);
-	b2Vec2 bodyPos = b2Vec2(PIXEL_TO_METERS(pos.getX()), PIXEL_TO_METERS(pos.getY()));
-	pbody->body->SetTransform(bodyPos, 0);
+    pos.setX(pos.getX() + texW / 2);
+    pos.setY(pos.getY() + texH / 2);
+    b2Vec2 bodyPos = b2Vec2(PIXEL_TO_METERS(pos.getX()), PIXEL_TO_METERS(pos.getY()));
+    pbody->body->SetTransform(bodyPos, 0);
 }
 
 Vector2D Enemy::GetPosition() {
-	b2Vec2 bodyPos = pbody->body->GetTransform().p;
-	Vector2D pos = Vector2D(METERS_TO_PIXELS(bodyPos.x), METERS_TO_PIXELS(bodyPos.y));
-	return pos;
+    b2Vec2 bodyPos = pbody->body->GetTransform().p;
+    Vector2D pos = Vector2D(METERS_TO_PIXELS(bodyPos.x), METERS_TO_PIXELS(bodyPos.y));
+    return pos;
 }
 
 void Enemy::ResetPath() {
-	Vector2D pos = GetPosition();
-	Vector2D tilePos = Engine::GetInstance().map.get()->WorldToMap(pos.getX(), pos.getY());
-	pathfinding->ResetPath(tilePos);
+    Vector2D pos = GetPosition();
+    Vector2D tilePos = Engine::GetInstance().map.get()->WorldToMap(pos.getX(), pos.getY());
+    pathfinding->ResetPath(tilePos);
 }
 
 void Enemy::OnCollision(PhysBody* physA, PhysBody* physB) {
-	switch (physB->ctype)
-	{
-	case ColliderType::PLAYER:
-		// Obtener posición del ENEMY
-		Vector2D enemyPos = GetPosition();
+    if (physB->ctype == ColliderType::PLAYER) {
+        Vector2D enemyPos = GetPosition();
+        b2Vec2 playerBodyPos = physB->body->GetPosition();
+        Vector2D playerPos(METERS_TO_PIXELS(playerBodyPos.x), METERS_TO_PIXELS(playerBodyPos.y));
 
-		// Obtener posición del PLAYER
-		b2Vec2 playerBodyPos = physB->body->GetPosition();
-		Vector2D playerPos(METERS_TO_PIXELS(playerBodyPos.x), METERS_TO_PIXELS(playerBodyPos.y));
-
-		// Verificar si el PLAYER está por encima del ENEMY
-		if (playerPos.getY() < enemyPos.getY() - (texH / 4)) {
-			LOG("Player is above the enemy - DESTROY");
-			Engine::GetInstance().entityManager.get()->DestroyEntity(this);
-		}
-		else {
-			LOG("Player collided but not from above");
-		}
-		break;
-	}
+        if (playerPos.getY() < enemyPos.getY() - (texH / 4)) {
+            LOG("Player is above the enemy - DESTROY");
+            Engine::GetInstance().entityManager.get()->DestroyEntity(this);
+        }
+        else {
+            LOG("Player collided but not from above");
+        }
+    }
 }
 
+void Enemy::OnCollisionEnd(PhysBody* physA, PhysBody* physB) {
+    if (physB->ctype == ColliderType::PLAYER) {
+        LOG("Collision ended with player");
+    }
+}
 
-void Enemy::OnCollisionEnd(PhysBody* physA, PhysBody* physB)
-{
-	switch (physB->ctype)
-	{
-	case ColliderType::PLAYER:
-		LOG("Collision player");
-		break;
-	}
+// Implement the SetParameters function
+void Enemy::SetParameters(pugi::xml_node parameters) {
+    this->parameters = parameters; // Simply assign the XML node to the member variable
 }
--- src/Enemy.h
+++ src/Enemy.h
@@ -10,44 +10,31 @@ struct SDL_Texture;
 class Enemy : public Entity
 {
 public:
+    Enemy();
+    virtual ~Enemy();
 
-	Enemy();
-	virtual ~Enemy();
+    bool Awake();
+    bool Start();
+    bool Update(float dt);
+    bool CleanUp();
+    void DrawPath();
 
-	bool Awake();
-
-	bool Start();
-
-	bool Update(float dt);
-
-	bool CleanUp();
-
-	void SetParameters(pugi::xml_node parameters) {
-		this->parameters = parameters;
-	}
-
-	void SetPosition(Vector2D pos);
-
-	Vector2D GetPosition();
-
-	void ResetPath();
-
-	void OnCollision(PhysBody* physA, PhysBody* physB);
-
-	void OnCollisionEnd(PhysBody* physA, PhysBody* physB);
-
-public:
+    void SetParameters(pugi::xml_node parameters);
+    void SetPosition(Vector2D pos);
+    Vector2D GetPosition();
+    void ResetPath();
+    void OnCollision(PhysBody* physA, PhysBody* physB);
+    void OnCollisionEnd(PhysBody* physA, PhysBody* physB);
 
 private:
+    SDL_Texture* texture;
+    const char* texturePath;
+    int texW, texH;
+    pugi::xml_node parameters;
+    Animation* currentAnimation = nullptr;
+    Animation idle;
+    PhysBody* pbody;
+    Pathfinding* pathfinding;
 
-	SDL_Texture* texture;
-	const char* texturePath;
-	int texW, texH;
-	pugi::xml_node parameters;
-	Animation* currentAnimation = nullptr;
-	Animation idle;
-	PhysBody* pbody;
-	Pathfinding* pathfinding;
-
-
+    bool DrawPathFlag = false; // Flag to control the path drawing
 };
